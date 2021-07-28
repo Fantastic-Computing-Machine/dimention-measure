105b2e327b27b83014ea7ee7e1542b0fab14b30f
ZEROCONNECT <s.nileshkm@gmail.com>
2021-07-28 17:12:28 +0530

    index_view update

--- views.py
+++ views.py
@@ -1,23 +1,36 @@
-from flask import Flask, render_template, redirect, session, request
+from flask import Flask, render_template, redirect, session, request,flash,url_for
 import random
 import string
 
 import database
 import app
 import helper
-
+import database
 app = user_auth = Flask(__name__)
 
 
 def index_view():
     print("INDEX-VIEW:", session)
+    session["projectNameList"] = ["asd","qqq"]
+    if request.method == "POST":
+        projectName = request.form.get("projectName")
+        print(projectName)
+        if projectName in session["projectNameList"]:
+            flash("Project name already exists! Please try with different name :)")
+            return redirect(request.url)
+        else:
+            md = database.MongoDatabase()
+            mdb = database.MongoDocumentCreator()
+            try:
+                md.postQuery(mdb.projectInitialization(projectName))
+            except Exception as e:
+                print(e)
+                # add 404 page
+            session["projectNameList"].append(str(projectName))
+            return redirect(url_for('record',projectName=projectName))
     return render_template('index.html')
 
 
-def records_view():
-    # try:
-    #     name = request.form['name']
-    #     length = request.form['length']
-    #     width = request.form['width']
-    #     rate = request.form['rate']
+def records_view(projectName):
+    
     return render_template('records.html')
