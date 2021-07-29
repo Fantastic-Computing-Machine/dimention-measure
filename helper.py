e3598eb3bd0dd8797f9dd61ae659f428ade996a6
ZEROCONNECT <s.nileshkm@gmail.com>
2021-07-30 00:23:36 +0530

    one time projectNameList

--- helper.py
+++ helper.py
@@ -7,13 +7,15 @@ def initialization():
     print()
     print("**********HELPER**************")
     projectNameList = []
-    md = database.MongoDatabase()
-    result = md.find({}, {"projectName": 2})
-    # print(result)
-    for i in result:
-        # print(i)
-        projectNameList.append(i["projectName"])
-    session["projectNameList"] = projectNameList
-    # print(projectNameList)
     print(session)
+    if("projectNameList" in session):
+        md = database.MongoDatabase()
+        result = md.find({}, {"projectName": 2})
+        # print(result)
+        for i in result:
+            # print(i)
+            projectNameList.append(i["projectName"])
+        session["projectNameList"] = projectNameList
+    else:
+        print(session)
     print("********************************")
