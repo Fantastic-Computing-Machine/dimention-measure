a804d92e9b8e7b815b8cb289b8c98092cff087ef
Aditya Agarwal <aditya.ag1234@gmail.com>
2021-09-26 20:33:20 +0530

    progress in expense, sum of paid and recieved

--- expense_urls.py
+++ expense_urls.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, redirect, url_for
 
-from expense_views import expense_home_view
+from expense_views import expense_home_view, payee_update_view, payee_delete_view
 
 expense_urls = Blueprint(__name__, "expense")
 
@@ -10,6 +10,16 @@ def expense_home():
     return expense_home_view()
 
 
-@expense_urls.route('/expense_home/success/')
+@expense_urls.route('/expense/success/')
 def expense_success():
     return redirect(url_for('expense_urls.expense_home'))
+
+
+@expense_urls.route('/expense/payee/delete/<payee_id>')
+def payee_delete(payee_id):
+    return payee_delete_view(payee_id)
+
+
+@expense_urls.route('/expense/payee/update/<payee_id>')
+def payee_update(payee_id):
+    return payee_update_view(payee_id)
--- expense_views.py
+++ expense_views.py
@@ -7,13 +7,16 @@ from database import SqlDatabase
 def expense_home_view():
     sql_obj = SqlDatabase()
 
-    payee_list_query = "SELECT payeeName FROM payees;"
+    payee_list_query = "SELECT * FROM payees;"
     payee_list = sql_obj.fetchRead(payee_list_query)
     print("PAYEE LIST: ", payee_list)
     expense_list_query = "SELECT * FROM expenses;"
-    # payee, amount, date_created, payment_status
+
     expense_list = sql_obj.fetchRead(expense_list_query)
     print("EXPENSE LIST: ", expense_list)
+
+    sum_query = "SELECT SUM(p1.amount) as sum_paid, SUM(p2.amount) as sum_gained FROM expenses p1, expenses p2 WHERE p1.payment_status='Paid' AND p2.payment_status='Recieved';"
+    sum_result = sql_obj.fetchRead(sum_query)
 
     if request.method == 'POST':
         print(request.form)
@@ -33,13 +36,26 @@ def expense_home_view():
         elif "userModal" in request.form:
             new_payee = request.form.get(
                 'newPayeeName').strip().replace(' ', "-")
-            print("USER MODAL: ", new_payee)
+            about = request.form.get('about')
+            print("USER MODAL: ", new_payee, about)
 
-            insert_expense_query = "insert into payees(payeeName) values('%s');" % (
-                str(new_payee))
+            insert_expense_query = "insert into payees(payeeName, about) values('%s', '%s');" % (
+                str(new_payee), str(about))
             insert_value = sql_obj.executeWrite(insert_expense_query)
             # Flashing message (successful and unsuccessful)
 
             return redirect(url_for('expense_urls.expense_success'))
 
-    return render_template('expense_home.html', payee_list=payee_list, expense_list=expense_list)
+    return render_template('expense_home.html', payee_list=payee_list, expense_list=expense_list, sum_result=sum_result)
+
+
+def payee_update_view(payee_id):
+    return redirect(url_for('expense_urls.expense_home'))
+
+
+def payee_delete_view(payee_id):
+    sql_obj = SqlDatabase()
+    delete_payee_query = "DELETE FROM 'payees' WHERE payeeId='%i'" % (
+        int(payee_id))
+    delete_payee = sql_obj.executeWrite(delete_payee_query)
+    return redirect(url_for('expense_urls.expense_home'))
