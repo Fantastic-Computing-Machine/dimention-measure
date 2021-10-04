a15b478d0c50edb0d71a4f4551daaa898c0323b1
Aditya Agarwal <aditya.ag1234@gmail.com>
2021-10-04 21:10:51 +0530

    update, delete of payee and exp

--- expense_urls.py
+++ expense_urls.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, redirect, url_for
 
 from expense_views import expense_home_view, payee_update_view, payee_delete_view
+from expense_views import expense_update_view, expense_delete_view, payee_expense_view
 
 expense_urls = Blueprint(__name__, "expense")
 
@@ -15,11 +16,28 @@ def expense_success():
     return redirect(url_for('expense_urls.expense_home'))
 
 
-@expense_urls.route('/expense/payee/delete/<payee_id>')
+@expense_urls.route('/payee/delete/<int:payee_id>')
 def payee_delete(payee_id):
+    print("DELETE payee_id: ", payee_id)
     return payee_delete_view(payee_id)
 
 
-@expense_urls.route('/expense/payee/update/<payee_id>')
+@expense_urls.route('/payee/update/<int:payee_id>')
 def payee_update(payee_id):
     return payee_update_view(payee_id)
+
+
+@expense_urls.route('/exp/delete/<int:expId>')
+def expense_delete(expId):
+    print("DELETE expense_id: ", expId)
+    return expense_delete_view(expId)
+
+
+@expense_urls.route('/exp/update/<int:expId>')
+def expense_update(expId):
+    return expense_update_view(expId)
+
+
+@expense_urls.route('/payee_expense/<string:payee>')
+def payee_expense(payee):
+    return payee_expense_view(payee)
