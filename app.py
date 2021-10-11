1b4641c6dad2ec9d8fea18a8f73885ab7af22c72
Aditya Agarwal <aditya.ag1234@gmail.com>
2021-10-11 18:18:35 +0530

    update moved to new page, routes recreated

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask.helpers import url_for
 from werkzeug.utils import redirect
 
 from views import index_view, records_view, delete_view, deleteProject_view
-from views import download_excel_view
+from views import download_excel_view, update_dimention_view
 
 from expense_urls import expense_urls
 import helper
@@ -15,7 +15,6 @@ app = Flask(__name__)
 
 app.register_blueprint(expense_urls, url_prefix="/expense/")
 
-app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URI
 app.config['SECRET_KEY'] = SECRET_KEY
 # app.secret_key = SECRET_KEY
 
@@ -29,7 +28,7 @@ def index():
     return index_view()
 
 
-@app.route('/record/<string:projectName>/<int:pid>', methods=["POST", "GET"])
+@app.route('/<string:projectName>/record/<int:pid>', methods=["POST", "GET"])
 def record(projectName, pid):
     if("projectNameList" not in session):
         helper.initialization()
@@ -38,20 +37,20 @@ def record(projectName, pid):
     return render_template("error_404.html")
 
 
-@app.route('/delete/<string:projectName>/<int:pid>/<rowNumber>/', methods=["POST", "GET"])
-def delete(projectName, pid, rowNumber):
+@app.route('/<string:projectName>/delete/<int:pid>_<int:dimid>/', methods=["POST", "GET"])
+def delete(projectName, pid, dimid):
     if("projectNameList" not in session):
         helper.initialization()
-    return delete_view(projectName, pid, rowNumber)
+    return delete_view(projectName, pid, dimid)
 
 
-@app.route('/success/<string:projectName>/<int:pid>', methods=["POST", "GET"])
+@app.route('/<string:projectName>/success/<int:pid>', methods=["POST", "GET"])
 def success(projectName, pid):
     print("success")
     return redirect(url_for('record', projectName=projectName, pid=pid))
 
 
-@app.route('/delete/<projectName>/', methods=["POST", "GET"])
+@app.route('/<string:projectName>/delete/', methods=["POST", "GET"])
 def deleteProject(projectName):
     sessionlist = session['projectNameList']
     sessionlist.remove(projectName)
@@ -60,11 +59,16 @@ def deleteProject(projectName):
     return deleteProject_view(projectName)
 
 
-@app.route('/download_excel/<projectName>/', methods=["POST", "GET"])
+@app.route('/<string:projectName>/download_excel/', methods=["POST", "GET"])
 def download_excel(projectName):
     # route to download to excel
-    print("Downloades-->", projectName)
+    print("Downloads-->", projectName)
     return download_excel_view(projectName)
+
+
+@app.route('/<string:projectName>/update/<int:pid>_<int:dimid>/', methods=["POST", "GET"])
+def update_dimention(projectName, pid, dimid):
+    return update_dimention_view(projectName, pid, dimid)
 
 
 @app.errorhandler(404)
