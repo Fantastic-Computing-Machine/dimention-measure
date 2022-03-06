7f9ef2ff393b74b313be271658911096274f04c8
Aditya Agarwal <aditya.ag1234@gmail.com>
2022-03-06 16:58:53 +0530

    excel file

--- Dimention_Measure/dimension/urls.py
+++ Dimention_Measure/dimension/urls.py
@@ -4,24 +4,30 @@ from django.urls import path
 from django.urls import include
 
 from .views import HomeView, ProjectView, UpdateDimensionView
-from .views import DeleteDimentionView
-from .views import DeleteProjectView
+from .views import DeleteDimentionView, DeleteProjectView
+from .views import download_excel_view
+
 
 urlpatterns = [
     path("", HomeView.as_view(), name="home"),
+
     # project_id, project_name
     path("project/<int:pk>/<str:project>/",
          ProjectView.as_view(), name="project_detail"),
 
+    # project_id, project_name
     path("project/<int:pk>/<str:project_name>/delete/project/",
-         # project_id, project_name
          DeleteProjectView, name="delete_project"),
 
     # dimention_id, project_name
     path("project/<int:pk>/<str:project_name>/update/dimention/",
          UpdateDimensionView.as_view(), name="update_dimention"),
 
+    # dimention_id, project_name
     path("project/<int:pk>/<int:project_id>/<str:project_name>/delete/dimention/",
-         # dimention_id, project_name
          DeleteDimentionView, name="delete_dimention"),
+
+    # project_id, project_name
+    path("excel/<int:project_id>/<str:project_name>/download/",
+         download_excel_view, name="download_excel_file"),
 ]
--- Dimention_Measure/dimension/views.py
+++ Dimention_Measure/dimension/views.py
@@ -1,7 +1,8 @@
-from django.forms.models import modelform_factory
-from django.db.models import Q, F
 from django.contrib.auth import get_user_model as user_model
 from django.contrib.auth.models import User
+from django.db.models import Q, F
+from django.forms.models import modelform_factory
+from django.http import JsonResponse
 from django.shortcuts import render, get_object_or_404
 from django.shortcuts import HttpResponseRedirect
 from django.views.generic import (
@@ -11,11 +12,12 @@ from django.views.generic import (
     DeleteView,
 )
 from django.urls import reverse_lazy, reverse
-from django.http import JsonResponse
-import re
-import datetime
 
-from .forms import NewProjectForm, NewDimensionForm,DeleteProjectForm
+import datetime
+from openpyxl import Workbook
+import re
+
+from .forms import NewProjectForm, NewDimensionForm, DeleteProjectForm
 from .forms import UpdateDimensionForm
 from .models import Project, Dimension
 
@@ -28,7 +30,8 @@ class HomeView(CreateView):
 
     def get_context_data(self, *args, **kwargs):
         context = super(HomeView, self).get_context_data(*args, **kwargs)
-        context["projects_list"] = Project.objects.filter(is_deleted=False).order_by('-created_on')
+        context["projects_list"] = Project.objects.filter(
+            is_deleted=False).order_by('-created_on')
         return context
 
     def post(self, request, **kwargs):
@@ -45,7 +48,8 @@ class ProjectView(CreateView):
 
     def get_context_data(self, *args, **kwargs):
         project = Project.objects.filter(pk=self.kwargs['pk'])[0]
-        dimentions = Dimension.objects.filter(project=project).filter(is_deleted=False)
+        dimentions = Dimension.objects.filter(
+            project=project).filter(is_deleted=False)
         kwargs['dimentions'] = dimentions
         kwargs['project'] = project
         kwargs['sum_sqm'] = sum(item.sqm for item in dimentions)
@@ -84,13 +88,75 @@ class UpdateDimensionView(UpdateView):
         request.POST._mutable = False
         return super(UpdateDimensionView, self).post(request, **kwargs)
 
-def DeleteProjectView(request, pk,project_name):
+
+def DeleteProjectView(request, pk, project_name):
     if request.method == 'POST':
-        project = Project.objects.filter(pk=pk).update(is_deleted=True,deleted_on=datetime.datetime.now())
+        project = Project.objects.filter(pk=pk).update(
+            is_deleted=True, deleted_on=datetime.datetime.now())
     return HttpResponseRedirect(reverse('home'))
 
-def DeleteDimentionView(request, pk,project_id,project_name):
+
+def DeleteDimentionView(request, pk, project_id, project_name):
     if request.method == 'POST':
-        dimension = Dimension.objects.filter(pk=pk).update(is_deleted=True,deleted_on=datetime.datetime.now())
-    return HttpResponseRedirect(reverse('project_detail', args=(project_id,project_name,)))
-    
+        dimension = Dimension.objects.filter(pk=pk).update(
+            is_deleted=True, deleted_on=datetime.datetime.now())
+    return HttpResponseRedirect(reverse('project_detail', args=(project_id, project_name,)))
+
+
+def download_excel_view(project_id, project_name):
+    date_time_obj = datetime.now()
+    current_date = date_time_obj.strftime('%x')
+    current_time = date_time_obj.strftime('%X')
+
+    filename = "media/excel/" + project_name + '_' + str(current_date).replace('/', "-") + \
+        '_' + str(current_time).replace(":", "-") + ".xlsx"
+
+    # create a workbook object
+    workbook = Workbook()
+    # create a worksheet
+    sheet = workbook.active
+    sheet.title = project_name
+
+    sheet.append(["Project Name", project_name])
+    sheet.append([""])
+    sheet.append(["Tag", "Length", "Width", "Area | sqm",
+                  "Area | sqft", "Rate", "Amount"])
+
+    dimention = Dimension.objects.filter(project__id=project_id)
+    print(dimention)
+
+    for item in session['proj_info'][1]:
+
+        item["length"] = str(item["length"]) + " m (" + \
+            str(round(item["length"]*3.281, 2)) + " ft.)"
+        item["width"] = str(item["width"]) + " m (" + \
+            str(round(item["width"]*3.281, 2)) + " ft.)"
+
+        sheet.append([item["tag"], item["length"],
+                      item["width"], item["sqm"], item["sqft"], item["rate"], item["amount"]])
+
+    sheet.append([""])
+    sheet.append(['Total sqm', session['proj_info'][2]])
+    sheet.append(['Total sqft', session['proj_info'][3]])
+    sheet.append(['Total amount*', session['proj_info'][4]])
+
+    sheet.append([""])
+    sheet.append(['*Calculated using metrics in sqft.'])
+
+    print(filename)
+    workbook.save(filename=str(filename))
+    workbook.close()
+
+    session.pop('proj_info')
+
+    # @ after_this_request
+    # def remove_file(response):
+    #     try:
+    #         os.remove(filename)
+    #         FileHandler.close()
+    #     except Exception as error:
+    #         app.logger.error(
+    #             "Error removing or closing downloaded file handle", error)
+    #     return response
+
+    # return FileResponse(open(path_to_file, 'rb'), as_attachment=True)
