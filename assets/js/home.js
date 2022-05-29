2ec76ca7d585ef05a0dfa77a0ab68bff3c475f5e
Aditya Agarwal <aditya.ag1234@gmail.com>
2022-05-30 03:25:41 +0530

    static

---
+++ assets/js/home.js
@@ -0,0 +1,22 @@
+// console.log('home.js');
+// $(document).ready(function() {
+//     $('.confirm_delete').click(function() {
+//         var id = $(this).attr('data-id');
+//         var message = 'Are you sure you want to delete ' + name + '?';
+//         if (confirm(message)) {
+//             $.ajax({
+//                 url: url,
+//                 type: 'DELETE',
+//                 data: {
+//                     '_token': token,
+//                     'id': id
+//                 },
+//                 success: function(data) {
+//                     if (data.success) {
+//                         window.location.reload();
+//                     }
+//                 }
+//             });
+//         }
+//     });
+// });
---
+++ assets/js/main.js
@@ -0,0 +1,82 @@
+console.log("main.js loaded");
+
+var length = document.getElementById("id_length");
+var width = document.getElementById("id_width");
+var rate = document.getElementById("id_rate");
+
+
+function areas() {
+    var len = parseFloat(length.value);
+    var wid = parseFloat(width.value);
+    var rt = parseFloat(rate.value);
+    var flag = false;
+
+    if (isNaN(len)) { len = 0; }
+    if (isNaN(wid)) { wid = 1; flag = true; }
+    if (isNaN(rt)) { rt = 0; }
+
+    var sqmtr = len * wid;
+    document.getElementById("sqm").value = sqmtr;
+
+    if (flag == true) {
+        var sqfeet = len * wid * 3.28;
+        document.getElementById("sqft").value = sqfeet;
+
+        var amount = sqfeet * rt
+        document.getElementById("amount").value = amount;
+
+        document.getElementById("sqm").value = 0.0;
+        // <!-- if you change "N/A" here then change the value in records.html file also -->
+        document.getElementById("sqft").value = 0.0;
+    } else {
+        var sqfeet = len * wid * 10.764;
+        document.getElementById("sqft").value = sqfeet;
+
+        var amount = sqfeet * rt
+        document.getElementById("amount").value = amount;
+    }
+}
+
+var meter = document.getElementById("mt");
+var feet = document.getElementById("ft");
+
+function mtr_ft() {
+    var len = parseFloat(meter.value);
+    if (isNaN(len)) { len = 0; }
+
+    var to_ft = len * 3.28;
+    document.getElementById("ft").value = to_ft;
+}
+
+function ft_mtr() {
+    var len = parseFloat(feet.value);
+    if (isNaN(len)) { len = 0; }
+
+    var to_mt = len * 0.3048;
+    document.getElementById("mt").value = to_mt;
+}
+
+$(document).ready(function () {
+    $(".delete_button").hover(function () {
+        $(this).children('img').attr('src', 'https://img.icons8.com/ios/50/000000/delete-forever--v2.gif');
+
+    }, function () {
+        $(this).children('img').attr("src", "https://img.icons8.com/ios/50/000000/delete-forever--v2.png");
+
+
+    });
+});
+
+$(document).ready(function () {
+    $(".setting").hover(function () {
+        $(this).children('img').attr('src', 'https://img.icons8.com/ios/28/000000/settings--v2.gif');
+
+    }, function () {
+        $(this).children('img').attr("src", "https://img.icons8.com/ios/28/000000/settings--v2.png");
+
+
+    });
+});
+
+var dt = new Date();
+document.getElementById('date-time').innerHTML = dt;
