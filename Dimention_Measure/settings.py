237220b9cab621a51a88fe187cefb06ad13b8310
Aditya Agarwal <aditya.ag1234@gmail.com>
2022-07-24 16:40:08 +0530

    login fixed and floating buttons

--- Dimention_Measure/settings.py
+++ Dimention_Measure/settings.py
@@ -175,6 +175,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = "home"
 LOGOUT_REDIRECT_URL = "home"
 
+LOGIN_URL = "login"
+
 PHONE_NUMBER_FORMAT = RegexValidator(regex=r"^\+?1?\d{8,15}$")
 
 STATE_CHOICES = (
