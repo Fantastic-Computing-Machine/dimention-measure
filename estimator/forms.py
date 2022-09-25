c529a298f8466a8abc49d6f4243bcda2e0cf83c4
Ubuntu <ubuntu@ip-10-0-11-124.ap-south-1.compute.internal>
2022-09-25 08:20:44 +0000

    phone number max length update and more

--- estimator/forms.py
+++ estimator/forms.py
@@ -71,7 +71,7 @@ class UpdateProjectForm(forms.ModelForm):
 
     client = forms.ModelChoiceField(
         required=True,
-        queryset=Client.objects.all(),
+        queryset=Client.objects.filter(is_deleted=False),
         widget=forms.Select(
             attrs={
                 "class": "form-select",
