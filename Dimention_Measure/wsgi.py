754d69ea1ba47c11efe1307a2c644d807d91003a
Aditya Agarwal <aditya.ag1234@gmail.com>
2022-11-14 00:25:01 +0530

    Update wsgi.py

--- Dimention_Measure/wsgi.py
+++ Dimention_Measure/wsgi.py
@@ -13,4 +13,9 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Dimention_Measure.settings')
 
-application = get_wsgi_application()
+# application = get_wsgi_application()
+
+
+from whitenoise.django import DjangoWhiteNoise
+
+application = DjangoWhiteNoise(application)
