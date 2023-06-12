d5853484eca0dad2378982e97958f19b7f99317c
Nilesh Km <s.nileshkm@gmail.com>
2023-06-13 00:18:33 +0530

    updates for eb

--- Dimention_Measure/prod_settings.py
+++ Dimention_Measure/prod_settings.py
@@ -154,11 +154,11 @@ WSGI_APPLICATION = 'Dimention_Measure.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.getenv['RDS_DB_NAME'],
-        'USER': os.getenv['RDS_USERNAME'],
-        'PASSWORD': os.getenv['RDS_PASSWORD'],
-        'HOST': os.getenv['RDS_HOSTNAME'],
-        'PORT': os.getenv['RDS_PORT'],
+        'NAME': os.getenv('RDS_DB_NAME'),
+        'USER': os.getenv('RDS_USERNAME'),
+        'PASSWORD': os.getenv('RDS_PASSWORD'),
+        'HOST': os.getenv('RDS_HOSTNAME'),
+        'PORT': os.getenv('RDS_PORT'),
     }
 }
 
