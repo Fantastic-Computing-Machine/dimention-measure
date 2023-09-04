025722b237e8f279b9a3cbf3fbbb3d7fdd633e7b
Nilesh Km <s.nileshkm@gmail.com>
2023-09-04 11:40:58 +0530

    updating wisgy with try catch

--- Dimention_Measure/wsgi.py
+++ Dimention_Measure/wsgi.py
@@ -13,17 +13,20 @@ from django.core.wsgi import get_wsgi_application
 # from whitenoise import WhiteNoise
 # from whitenoise.django import DjangoWhiteNoise
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Dimention_Measure.settings')
+try :
+
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Dimention_Measure.settings')
 
 
-application = get_wsgi_application()
+    application = get_wsgi_application()
 
 
-# application = DjangoWhiteNoise(application)
+    # application = DjangoWhiteNoise(application)
 
-app = application
+    app = application
 
-
+except Exception as e :
+    print("Error in wsgi.py : ",e)
 
 # from my_project import MyWSGIApp
 
