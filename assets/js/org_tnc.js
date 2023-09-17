f74b0b0f039f3558b335094d123cc5787185cba6
Nilesh Km <s.nileshkm@gmail.com>
2023-09-17 13:09:42 +0530

    org_details and estimate enabled fix

--- assets/js/org_tnc.js
+++ assets/js/org_tnc.js
@@ -1,26 +1,15 @@
-document.getElementById('id_company_name').disabled = true;
-document.getElementById('id_manager_name').disabled = true;
-document.getElementById('id_email').disabled = true;
-document.getElementById('id_phoneNumber').disabled = true;
-document.getElementById('id_address_1').disabled = true;
-document.getElementById('id_address_2').disabled = true;
-document.getElementById('id_landmark').disabled = true;
-document.getElementById('id_town_city').disabled = true
-document.getElementById('id_zip_code').disabled = true;
-document.getElementById('id_state').disabled = true;
-document.getElementById('save_organization_details').disabled = true;
+const form = document.forms['org_details_form'];
+const elements = form.elements;
 
+for (let i = 0; i < elements.length; i++) {
+  elements[i].disabled = true;
+}
 
 function edit_organization_form() {
-    document.getElementById('id_company_name').disabled = false;
-    document.getElementById('id_manager_name').disabled = false;
-    document.getElementById('id_email').disabled = false;
-    document.getElementById('id_phoneNumber').disabled = false;
-    document.getElementById('id_address_1').disabled = false;
-    document.getElementById('id_address_2').disabled = false;
-    document.getElementById('id_landmark').disabled = false;
-    document.getElementById('id_town_city').disabled = false;
-    document.getElementById('id_zip_code').disabled = false;
-    document.getElementById('id_state').disabled = false;
-    document.getElementById('save_organization_details').disabled = false;
+    const form = document.forms['org_details_form'];
+    const elements = form.elements;
+
+    for (let i = 0; i < elements.length; i++) {
+    elements[i].disabled = false;
+    }
 }
